0f3c31b28cdbb41719d9a8f851924367b001a95e
nshadov <nshadov.eq@gmail.com>
2018-02-12 11:30:07 +0100

    add logging feature

--- artillery-iptables-cleanup.go
+++ artillery-iptables-cleanup.go
@@ -5,15 +5,23 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func getRulesID(s []byte) []int {
+func getRulesID(s []byte, filename string) []int {
 	reader := bytes.NewReader(s)
 	scanner := bufio.NewScanner(reader)
+
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	timestamp := time.Now().String()
 
 	var ids []int
 
@@ -21,10 +29,19 @@ func getRulesID(s []byte) []int {
 		s := scanner.Text()
 		if strings.Contains(s, "DROP") {
 			fmt.Printf("%s\n", s)
+
+			if _, err := f.Write([]byte(timestamp + ": " + s + "\n")); err != nil {
+				log.Fatal(err)
+			}
+
 			i := strings.Split(s, " ")[0]
 			x, _ := strconv.Atoi(i)
 			ids = append(ids, x)
 		}
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
 	}
 
 	return ids
@@ -46,6 +63,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ids := getRulesID(out)
+	ids := getRulesID(out, "artillery_cleanup.log")
 	removeIptablesIDS(ids)
 }
